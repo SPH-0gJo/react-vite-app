f8c2fb96f42f7508b8b495d767631d8e02408f16
bokyung <yjjo@sphinfo.co.kr>
2024-03-21 15:50:20 +0900

    config. init 1: react + ts + vite 설정

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React-Vite-App</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('wrapper'));
