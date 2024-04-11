ad40868322b35d0ca1408efe75d57b6a9c32c68a
bokyung <yjjo@sphinfo.co.kr>
2024-04-11 16:37:52 +0900

    config. 번들링시 경로 찾을수있도록 매핑 설정

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+  resolve: {
+    alias: [
+      //    { find: 'axios', replacement: path.resolve(__dirname, './node_modules/axios/') },
+      { find: 'app', replacement: path.resolve(__dirname, './src/app/') },
+      { find: 'assets', replacement: path.resolve(__dirname, './public/assets/') },
+      { find: 'shared', replacement: path.resolve(__dirname, './src/shared/') },
+      //    { find: 'tests', replacement: path.resolve(__dirname, './tests/') },
+    ],
+  },
 });
