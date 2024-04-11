f61ab80aee15694d9f8aaf35a0b2a847cbd828ce
bokyung <yjjo@sphinfo.co.kr>
2024-04-11 16:44:10 +0900

    style. style 관련한 파일을 추가

---
+++ src/shared/styles/styled/common.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background-color: var(--white-a100);
+  height: 5.625rem;
+  box-shadow: 0 0 4px 0 rgba(0, 0, 0, 0.1);
+  grid-area: header;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0.625rem 1.25rem;
+  z-index: 999;
+  em {
+    font-size: 1.5rem;
+    margin-right: 0.88rem;
+  }
+`;
---
+++ src/shared/types/global.d.ts
@@ -0,0 +1,12 @@
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.css';
+declare module '*.sass';
+declare module '*.scss';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
